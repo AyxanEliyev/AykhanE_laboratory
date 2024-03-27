6380611c6a0a938123b44b9130111e99e422ccc0
Arzumanli <arzumanli.aqil@gmail.com>
2024-03-27 14:18:19 +0400

    lab6_Ayxan_Vurmacedveli

---
+++ mart/27.03lab6/lab6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+echo ("<table>");
+    for($i=0;$i<=10;$i++){
+    for($j=1;$j<=9;$j++){
+    echo "$i * $j =". $j*$i . "&emsp;";
+    }
+    echo "<br />";
+    }
+        
+echo ("</table>");
+
+?> 
+
+</body>
+</html>
+
